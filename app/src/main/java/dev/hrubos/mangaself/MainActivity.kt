ddf9de4cdafcc6537f18793ab2f7d92c63e1b870
Ondřej Hruboš <hruboson@gmail.com>
2025-10-11 10:10:41 +0200

    feat(ui): fix status bar padding

--- app/src/main/java/dev/hrubos/mangaself/MainActivity.kt
+++ app/src/main/java/dev/hrubos/mangaself/MainActivity.kt
@@ -1,9 +1,21 @@
 package dev.hrubos.mangaself
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import dev.hrubos.mangaself.navigation.AppNavigation
 import dev.hrubos.mangaself.ui.theme.MangaselfTheme
 import dev.hrubos.mangaself.viewmodel.ProfileViewModel
@@ -21,9 +33,24 @@ class MainActivity : ComponentActivity() {
             Log.d("MainActivity", "Profiles cleared successfully")
         }*/
 
+        // disable edge-to-edge on newer phones
+        window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        WindowInsetsControllerCompat(window, window.decorView).systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
         setContent {
             MangaselfTheme {
-                AppNavigation(profileViewModel, shelfViewModel)
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(WindowInsets.statusBars.asPaddingValues())
+                ) {
+                    AppNavigation(profileViewModel, shelfViewModel)
+                }
             }
         }
     }
